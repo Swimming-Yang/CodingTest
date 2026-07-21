a467c58a35252c2a24902cb0962011f2fcd928a5
Swimming_Yang <ysw1mst@gmail.com>
2026-07-21 10:01:18 +0900

    [level 0] Title: n의 배수, Time: 0.01 ms, Memory: 5.02 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181937. n의 배수/n의 배수.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int solution(int num, int n) {
+    int answer = 0;
+    if (num % n == 0) answer = 1;
+
+    else answer = 0;
+
+
+    return answer;
+}
